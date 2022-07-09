6335d714fbdbf40896c0d0b42d89cc2be204c082
Damien Schackemy <damien.schackemy@gmail.com>
2022-07-09 19:14:26 +0200

    correction localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -4,6 +4,9 @@ const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 const id = urlParams.get("id")
 
+const quantityPicked = document.querySelector("#quantity")
+const colorPicked = document.querySelector("#color")
+
 //on recupère les données des produits dans l'API
 
 fetch(`http://localhost:3000/api/products/${id}`)
@@ -11,13 +14,6 @@ fetch(`http://localhost:3000/api/products/${id}`)
 .then (res => Data(res))
 
 function Data(canape) {
-    //const altTxT = canape.altTxt
-    //const colors = canape.colors
-    //const description = canape.description
-    //const imageUrl = canape.imageUrl
-    //const name = canape.name
-    //const price = canape.price
-    //const _id = canape._id
     const {altTxt, colors, description, imageUrl, name, price,} = canape
     imgUrl = imageUrl
     altText = altTxt
@@ -28,7 +24,84 @@ function Data(canape) {
     createPrice (price)
     createDescription(description)
     createColors(colors)
+
+    addToCart(canape)
 }
+//On Ajoute au panier
+function addToCart(canape) {
+    const sendToCart = document.querySelector("#addToCart");
+
+    // verification conditions 
+    sendToCart.addEventListener("click", (event) => {
+        if (quantityPicked.value > 0  && quantityPicked <= 100) {
+            
+            // choix de la quantité
+            let choixQuantity = quantityPicked.value;
+
+            //choix de la couleur
+            let choixColor = colorPicked.value;
+
+
+            let productSettings = {
+                idProduct : id,
+                colorProduct : choixColor,
+                quantityProduct : +choixQuantity,
+                nameProduct : canape.name,
+                prixProduct : canape.price,
+                descriptionProduct : canape.description,
+                imgProduct : canape.imageUrl,
+                altImg : canape.altTxt,
+            };
+
+            //Initialisation du localStorage
+            let productInLocalStorage = JSON.parse(localStorage.getItem(canape));
+
+            //ouverture pop-up de confirmation
+            const popupConfirmation = () => {
+                if (
+                window.confirm(`Votre commande de ${choixQuantity} ${canape.name} ${choixColor} est ajoutée au panier
+            Pour consulter votre panier, cliquez sur OK`)
+                ) {
+                window.location.href = "cart.html";
+                }
+            };
+
+            //importation dans le localStorage
+
+                // Si panier avec au moins un article
+            if (productInLocalStorage) {
+                const productFind = productInLocalStorage.find (
+                    (el) =>
+                    el.id === id && el.colorProduct === choixColor
+                );
+
+                //si le produit commandé deja présent
+            if (productFind) {
+                let newQuantity =
+                    parseInt(productSettings.quantityProduct) +
+                    parseInt(productFind.quantityProduct);
+                    productFind.quantityProduct = newQuantity;
+            localStorage.setItem(canape, JSON.stringify(productInLocalStorage));
+            popupConfirmation();
+            
+            // si produit pas dans le panier
+            }else{
+                productInLocalStorage.push(productSettings);
+                localStorage.setItem(canape, JSON.stringify(productInLocalStorage));
+                popupConfirmation();
+            }
+            //Si panier est vide
+            }else {
+                productInLocalStorage = [];
+                productInLocalStorage.push(productSettings);
+                localStorage.setItem(canape, JSON.stringify(productInLocalStorage));
+                popupConfirmation();
+            }
+        }
+    })
+}
+
+
 //insertion de l'image 
 function createImage(imageUrl, altTxt) {
     const image = document.createElement("img")
@@ -67,45 +140,3 @@ function createColors(colors){
     }
 }
 
-const button = document.querySelector("#addToCart")
-if (button != null) {
-    button.addEventListener("click", handleClick)
-}
-function handleClick () {
-        const color = document.querySelector("#colors").value
-        const quantity = document.querySelector("#quantity").value
-
-        //on verifie que les valeur saisie sont juste sinon message alert
-        if (isOrderInvalid(color, quantity)) return
-
-        saveOrder(color, quantity)
-        //on redirige vers le panier
-        redirectToCart()
-
-}
-//fonction de verification de la commande avec apparition d'une pop-up au cas ou le choix est invalide
-function isOrderInvalid(color, quantity) {
-    if (color == null || color == "" || quantity == null || quantity == 0) {
-        alert ("Veuillez selectionner une couleur et une quantité")
-        return true
-    }
-}
-//fonction de sauvegarde du choix produit dans le localStorage
-function saveOrder (color, quantity) {
-    const key = `${id}-${color}`
-    const data = {
-        id: id,
-        color: color,
-        quantity: Number(quantity),
-        price: itemPrice,
-        imageUrl : imgUrl,
-        altTxT: altText,
-        name: artName,
-    }
-    localStorage.setItem(key, JSON.stringify(data))
-}
-//fonction de redirection
-function redirectToCart() {
-    alert ("vos produits sont ajoutés au panier, cliquez sur Ok pour le consulter")
-    window.location.href = "cart.html"
-}
